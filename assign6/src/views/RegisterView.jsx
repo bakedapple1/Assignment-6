b32e269cef2758752ea66ef0dd145ac244d0169c
Daniel <bingrongcao@gmail.com>
2025-05-13 15:14:12 -0400

    minor changes to RegisterView

--- assign6/src/views/RegisterView.jsx
+++ assign6/src/views/RegisterView.jsx
@@ -15,6 +15,20 @@ function RegisterView() {
         password: '',
         confPass: ''
     });
+    const [genresArray] = useState([
+        { genre: "Action", id: 28 },
+        { genre: "Adventure", id: 12 },
+        { genre: "Animation", id: 16 },
+        { genre: "Crime", id: 80 },
+        { genre: "Family", id: 10751 },
+        { genre: "Fantasy", id: 14 },
+        { genre: "History", id: 36 },
+        { genre: "Horror", id: 27 },
+        { genre: "Mystery", id: 9648 },
+        { genre: "Sci-Fi", id: 878 },
+        { genre: "War", id: 10752 },
+        { genre: "Western", id: 37 }
+    ]);
 
     function createAccount(event) {
         event.preventDefault();
