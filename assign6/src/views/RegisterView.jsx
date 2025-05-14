0eddee12d538a5f55ad1960eaf72a6c7e22a2488
Daniel <bingrongcao@gmail.com>
2025-05-14 13:53:25 -0400

    Started adding genre preferences to RegisterView

--- assign6/src/views/RegisterView.jsx
+++ assign6/src/views/RegisterView.jsx
@@ -6,6 +6,7 @@ import Footer from "../components/Footer";
 import "./RegisterView.css";
 
 function RegisterView() {
+    let selectedGenres = Array(12).fill(false);
     const navigate = useNavigate();
     const { userData, setUserData } = useStoreContext();
     const [userInfo, setUserInfo] = useState({
@@ -30,6 +31,11 @@ function RegisterView() {
         { genre: "Western", id: 37 }
     ]);
 
+    function setPreferences(idx) {
+        selectedGenres[idx] = !selectedGenres[idx];
+        console.log(selectedGenres);
+    }
+
     function createAccount(event) {
         event.preventDefault();
 
@@ -53,20 +59,31 @@ function RegisterView() {
             <Header />
             <div className="register">
                 <div className="register-menu">
-                    <h1 className="register-label">Sign Up</h1>
-                    <form className="register-form" id="register-form" onSubmit={(event) => { createAccount(event) }}>
-                        <label htmlFor="reg-first-name" className="reg-input-label">First Name:</label>
-                        <input type="text" name="reg-first-name" className="reg-input" id="reg-first-name" value={userInfo.firstName} onChange={(event) => { setUserInfo((prev) => ({ ...prev, firstName: event.target.value })) }} required />
-                        <label htmlFor="reg-last-name" className="reg-input-label">Last Name:</label>
-                        <input type="text" name="reg-last-name" className="reg-input" id="reg-last-name" value={userInfo.lastName} onChange={(event) => { setUserInfo((prev) => ({ ...prev, lastName: event.target.value })) }} required />
-                        <label htmlFor="reg-email" className="reg-input-label">Email:</label>
-                        <input type="email" name="reg-email" className="reg-input" id="reg-email" value={userInfo.email} onChange={(event) => { setUserInfo((prev) => ({ ...prev, email: event.target.value })) }} required />
-                        <label htmlFor="reg-pass" className="reg-input-label">Password:</label>
-                        <input type="password" name="reg-pass" className="reg-input" id="reg-pass" value={userInfo.password} onChange={(event) => { setUserInfo((prev) => ({ ...prev, password: event.target.value })) }} required />
-                        <label htmlFor="reg-conf-pass" className="reg-input-label">Confirm Password:</label>
-                        <input type="password" name="reg-conf-pass" className="reg-input" id="reg-conf-pass" value={userInfo.confPass} onChange={(event) => { setUserInfo((prev) => ({ ...prev, confPass: event.target.value })) }} required />
-                        <input type="submit" value="Register" className="submit-button" id="reg-submit" />
-                    </form>
+                    <div>
+                        <h1 className="register-label">Sign Up</h1>
+                        <form className="register-form" id="register-form" onSubmit={(event) => { createAccount(event) }}>
+                            <label htmlFor="reg-first-name" className="reg-input-label">First Name:</label>
+                            <input type="text" name="reg-first-name" className="reg-input" id="reg-first-name" value={userInfo.firstName} onChange={(event) => { setUserInfo((prev) => ({ ...prev, firstName: event.target.value })) }} required />
+                            <label htmlFor="reg-last-name" className="reg-input-label">Last Name:</label>
+                            <input type="text" name="reg-last-name" className="reg-input" id="reg-last-name" value={userInfo.lastName} onChange={(event) => { setUserInfo((prev) => ({ ...prev, lastName: event.target.value })) }} required />
+                            <label htmlFor="reg-email" className="reg-input-label">Email:</label>
+                            <input type="email" name="reg-email" className="reg-input" id="reg-email" value={userInfo.email} onChange={(event) => { setUserInfo((prev) => ({ ...prev, email: event.target.value })) }} required />
+                            <label htmlFor="reg-pass" className="reg-input-label">Password:</label>
+                            <input type="password" name="reg-pass" className="reg-input" id="reg-pass" value={userInfo.password} onChange={(event) => { setUserInfo((prev) => ({ ...prev, password: event.target.value })) }} required />
+                            <label htmlFor="reg-conf-pass" className="reg-input-label">Confirm Password:</label>
+                            <input type="password" name="reg-conf-pass" className="reg-input" id="reg-conf-pass" value={userInfo.confPass} onChange={(event) => { setUserInfo((prev) => ({ ...prev, confPass: event.target.value })) }} required />
+                            <input type="submit" value="Register" className="submit-button" id="reg-submit" />
+                        </form>
+                    </div>
+
+                    <div className="checkbox-list">
+                        {genresArray.map((genreCheck, index) => (
+                            <div className="checkbox">
+                                <label htmlFor={`check${genreCheck.id}`}>{genreCheck.genre}</label>
+                                <input type="checkbox" key={genreCheck.id} id={`check${genreCheck.id}`} className="reg-genres" onChange={() => setPreferences(index)} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
             <Footer />
